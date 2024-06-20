972be857c8afa07cb16d6f9f5be1f50033c54e0d
nbryleibanez <nbryleibanez@gmail.com>
2024-06-20 11:17:03 +0800

    navbar progress

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import HabitList from "@/components/habit-list";
 import ProfileCard from "@/components/profile-card";
+import NavBar from "@/components/nav-bar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col gap-4">
+    <main className="relative flex min-h-screen flex-col gap-4">
       <h1 className="text-lg font-bold">Home</h1>
       <ProfileCard />
       <HabitList />
+      <NavBar />
     </main>
   );
 }
--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -1,11 +1,30 @@
 import Link from "next/link";
 
+import { PlusIcon, HomeIcon, PersonIcon } from "@radix-ui/react-icons";
+
 export default function NavBar() {
   return (
-    <div>
-      <Link href="/">Home</Link>
-      <Link href="/create">Create</Link>
-      <Link href="/friends">Friends</Link>
+    <div className="absolute bottom-6 w-full flex justify-center">
+      <div className="h-16 w-fit px-3 py-2 flex gap-6 bg-white rounded-full">
+        <Link
+          className="h-12 w-12 flex justify-center items-center bg-background rounded-full"
+          href="/"
+        >
+          <HomeIcon className="h-7 w-7" color="#999999" />
+        </Link>
+        <Link
+          className="h-12 w-12 flex justify-center items-center bg-background rounded-full"
+          href="/create"
+        >
+          <PlusIcon className="h-7 w-7" color="#999999" />
+        </Link>
+        <Link
+          className="h-12 w-12 flex justify-center items-center bg-background rounded-full"
+          href="/friends"
+        >
+          <PersonIcon className="h-7 w-7" color="#999999" />
+        </Link>
+      </div>
     </div>
   );
 }
