09851151913df246ef784fca43c88733c7ffa517
nbryleibanez <nbryleibanez@gmail.com>
2024-06-25 11:19:14 +0800

    file restructuring

--- app/(with-navbar)/page.tsx
+++ app/(with-navbar)/page.tsx
@@ -1,6 +1,5 @@
 import HabitList from "@/components/habit-list";
 import ProfileCard from "@/components/profile-card";
-import NavBar from "@/components/nav-bar";
 
 export default function Home() {
   return (
@@ -8,7 +7,6 @@ export default function Home() {
       <h1 className="text-lg font-bold">Home</h1>
       <ProfileCard />
       <HabitList />
-      <NavBar />
     </main>
   );
 }
---
+++ app/(wo-navbar)/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Bits",
+  description: "Your research-based habit tracker.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} p-5`}>{children}</body>
+    </html>
+  );
+}
