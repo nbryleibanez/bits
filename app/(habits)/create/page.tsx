a41e82e7171a2e31a7de36b11df499ae113f62f7
nbryleibanez <nbryleibanez@gmail.com>
2024-07-23 13:17:58 +0800

    create habit page refinements

--- app/(habits)/create/page.tsx
+++ app/(habits)/create/page.tsx
@@ -4,22 +4,24 @@ import NavBar from "@/components/nav-bar";
 export default function CreatePage() {
   return (
     <main>
-      <h1>Choose a type of habit.</h1>
-      <div>
-        <Link href="/create/basic">
+      <h1 className="font-semibold mb-4">Choose a type</h1>
+      <div className="flex flex-col gap-4">
+        <Link className="bg-white rounded-3xl px-8 py-6" href="/create/basic">
           <h2 className="text-xl font-medium">Basic Habit</h2>
-          <p>A habit that is commonly used in habit-tracking applications.</p>
+          <p className="text-sm font-light">
+            A habit that is commonly used in habit-tracking applications.
+          </p>
         </Link>
-        <Link href="/create/cue">
+        <Link className="bg-white rounded-3xl px-8 py-6" href="/create/cue">
           <h2 className="text-xl font-medium">Cue-Based Habit</h2>
-          <p>
+          <p className="text-sm font-light">
             A habit triggered by a context cue. Follows the literature around
             habits.
           </p>
         </Link>
-        <Link href="/create/duo">
+        <Link className="bg-white rounded-3xl px-8 py-6" href="/create/duo">
           <h2 className="text-xl font-medium">Duo Habit</h2>
-          <p>Form a habit with a partner.</p>
+          <p className="text-sm font-light">Form a habit with a partner.</p>
         </Link>
       </div>
       <NavBar />
