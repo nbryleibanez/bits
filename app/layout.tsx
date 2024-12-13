92f887d9a5982522fe6f97b7c75db89dc6b1ebf4
Nhyl Bryle Ibanez <nbryleibanez@gmail.com>
2024-12-14 02:43:21 +0800

    service worker registration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { ServiceWorkerRegistration } from '@/components/service-worker-registration'
 import "@/app/globals.css";
 
 import { Toaster } from "@/components/ui/toaster";
@@ -26,6 +27,7 @@ export default function RootLayout({
         </TooltipProvider>
         <Toaster />
         <SpeedInsights />
+        <ServiceWorkerRegistration />
       </body>
     </html>
   );
