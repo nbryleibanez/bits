008c7ffb2b4d861e377d057bfe90b56fb749dfcc
Nhyl Bryle Ibanez <nbryleibanez@gmail.com>
2024-12-31 03:45:00 +0800

    pwa attempt

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon-192x192.png" />
+        <meta name="theme-color" content="#000000" />
+      </head>
       <body
         className={`${inter.className} w-full min-h-dvh max-h-dvh relative`}
       >
