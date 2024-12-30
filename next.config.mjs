de4a15af2df285a419bb22922a7112491c614410
Nhyl Bryle Ibanez <nbryleibanez@gmail.com>
2024-12-31 03:50:30 +0800

    pwa attempt

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,31 @@
+import withPWA from "next-pwa";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-}
+  reactStrictMode: true,
+};
 
-if (process.env.NODE_ENV === 'development' || process.env.NEXT_PUBLIC_ENV === 'staging') {
-  nextConfig.logging = {
+// Add PWA configuration
+const pwaConfig = withPWA({
+  dest: "public", // Output directory for service worker
+  register: true, // Automatically register service worker
+  skipWaiting: true, // Activate service worker immediately
+  scope: "/app",
+  sw: "service-worker.js",
+  disable: process.env.NODE_ENV === "development", // Disable in development mode
+})(nextConfig);
+
+// Additional configuration based on environment
+if (
+  process.env.NODE_ENV === "development" ||
+  process.env.NEXT_PUBLIC_ENV === "staging"
+) {
+  pwaConfig.logging = {
     fetches: {
       fullUrl: true,
       hmrRefreshes: true,
-    }
+    },
   };
-};
+}
 
-export default nextConfig;
+export default pwaConfig;
