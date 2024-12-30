73aa796d0b21d57f259e48d24876421c4b6bb202
Nhyl Bryle Ibanez <nbryleibanez@gmail.com>
2024-12-31 05:20:49 +0800

    pwa fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,11 @@
-import withPWA from "next-pwa";
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+const nextConfig = {};
 
-// Add PWA configuration
-const pwaConfig = withPWA({
-  dest: "public", // Output directory for service worker
-  register: true, // Automatically register service worker
-  skipWaiting: true, // Activate service worker immediately
-  scope: "/",
-  sw: "service-worker.js",
-  disable: process.env.NODE_ENV === "development", // Disable in development mode
-})(nextConfig);
-
-// Additional configuration based on environment
 if (
   process.env.NODE_ENV === "development" ||
   process.env.NEXT_PUBLIC_ENV === "staging"
 ) {
-  pwaConfig.logging = {
+  nextConfig.logging = {
     fetches: {
       fullUrl: true,
       hmrRefreshes: true,
@@ -28,4 +13,4 @@ if (
   };
 }
 
-export default pwaConfig;
+export default nextConfig;
